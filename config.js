8823e1a1c5ca17c304b63418fdce8f26599a9bd4
cmgouveiacultura-glitch <cmgouveiacultura@gmail.com>
2026-09-11 14:52:13 +0100

    Atualiza cache da homepage institucional

--- config.js
+++ config.js
@@ -5,10 +5,10 @@ window.ASSOCIATIVISMO_ENDPOINT = '';
 (function(){
   var css=document.createElement('link');
   css.rel='stylesheet';
-  css.href='preview-match-v2.css?v=2';
+  css.href='preview-match-v2.css?v=3';
   document.head.appendChild(css);
   var js=document.createElement('script');
-  js.src='preview-match-v2.js?v=2';
+  js.src='preview-match-v2.js?v=3';
   js.defer=true;
   document.head.appendChild(js);
 })();
